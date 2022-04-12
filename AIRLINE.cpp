19c89a99082d7911c59f6124d0e5418f65766f10
adarshak47 <59947795+adarshak47@users.noreply.github.com>
2022-04-12 20:51:50 +0530

    Create AIRLINE.cpp

---
+++ AIRLINE.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d,e;
+        cin>>a>>b>>c>>d>>e;
+        if(((a+b)<=d && c<=e) || ((a+c)<=d && b<=e) || ((c+b)<=d && a<=e))
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+	return 0;
+}
