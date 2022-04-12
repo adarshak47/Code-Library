5095c73518c63cd3374c0885cbe4532d1e5fb934
adarshak47 <59947795+adarshak47@users.noreply.github.com>
2022-04-12 21:06:19 +0530

    Create SOCKS1.cpp

---
+++ SOCKS1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(a==b or b==c or a==c)
+	cout<<"YES"<<endl;
+	else
+	cout<<"NO"<<endl;
+	return 0;
+}
