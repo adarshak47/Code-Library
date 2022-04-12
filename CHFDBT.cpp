5c9ecb522adb3c65d25cc5214208edf3d217e99b
adarshak47 <59947795+adarshak47@users.noreply.github.com>
2022-04-12 20:47:05 +0530

    Create CHFDBT.cpp

---
+++ CHFDBT.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n,ans;
+        cin>>n;
+        n++;
+        n=n>>1;
+        cout<<n<<endl;
+    }
+	return 0;
+}
+
